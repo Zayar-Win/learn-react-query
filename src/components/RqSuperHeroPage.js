9359243ba6c2666bdf1b523374f4ceba0d209a03
zayarwin <zayarwin7751@gmail.com>
2022-03-11 23:05:53 +0630

    final

--- src/components/RqSuperHeroPage.js
+++ src/components/RqSuperHeroPage.js
@@ -3,17 +3,19 @@ import React from "react";
 import { useQuery } from "react-query";
 
 const RqSuperHeroPage = () => {
-  const { isLoading, data } = useQuery(
-    "superheroes",
-    () => {
+  const { isLoading, data, isError, error } =
+    useQuery("superheroes", () => {
       return axios.get(
-        "http://localhost:4000/superheros"
+        "http://localhost:4000/superheros1"
       );
-    }
-  );
+    });
   if (isLoading) {
     return <h3>Loading...</h3>;
   }
+  if (isError) {
+    return <h3>{error.message}</h3>;
+  }
+
   return (
     <div>
       <h2>RqSuperHeroPage</h2>
