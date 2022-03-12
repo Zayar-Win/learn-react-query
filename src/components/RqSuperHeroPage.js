2d2f5bb64cc3fc4539aca1a5a47d386b0b1d6f15
zayarwin <zayarwin7751@gmail.com>
2022-03-12 20:50:41 +0630

    final

--- src/components/RqSuperHeroPage.js
+++ src/components/RqSuperHeroPage.js
@@ -17,8 +17,20 @@ const RqSuperHeroPage = () => {
         //when the user redirect to the page again within 5mins it will show the cache data
         //it will not set loading to true but is fetch in background if data change it will update the data but it work in background
         //we can change the cachetime by adding third parameter in useQuery options
-        cacheTime: 5000,
-        staleTime: 5000,
+        // cacheTime: 5000,
+        //react-query default staletime is 0s
+        //with in staletime react-query will not fetch data even in background
+        // staleTime: 5000,
+        //the default value of refetchOnMount is true it mean if the component mount
+        //the query will fetch automatically
+        //when you change to false it will fetch the first time but it will not fetch in anoter time.
+        //always it same to true;
+        // refetchOnMount: false, //it can change to false and "always"
+        //the default value of refetchOnWindowFocus is false,
+        //when we set to true it will fetch the data when window on focus it mean when we change the data
+        //it will directly update the data
+        //when we set to false it will update the component when refresh the page;
+        refetchOnWindowFocus: false,
       }
     );
   if (isLoading) {
